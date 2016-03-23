81d3587488d65d079c654847dcf7ec2a3aa65661
Jesus <jesus@trantor>
2016-03-22 22:20:21 -0600

    Agregado modelo de Eventos 	modified:   eventos/models.py

--- AgendaDicyg/eventos/models.py
+++ AgendaDicyg/eventos/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Evento(models.Model):
+    id_event = models.AutoField(primary_key=True)
+    id_us = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_us')
+    id_per = models.ForeignKey('Persona', models.DO_NOTHING, db_column='id_per')
+    event_inicio = models.DateTimeField()
+    event_fin = models.DateTimeField()
+    event_descripcion = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'evento'
+
